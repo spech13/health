5f3bc8a21bda8edf442716f842c5b82189700bc7
spech13 <es-popov2@yandex.ru>
2023-10-09 23:30:23 +0300

    Add models MedicineType, Disease, Contraindication, Medicine

--- apps/pharmacy/admin.py
+++ apps/pharmacy/admin.py
@@ -1,7 +1,38 @@
 from django.contrib import admin
-from apps.pharmacy.models import Tablet
+from apps.pharmacy.models import (
+    MedicineForm,
+    MedicineType,
+    Disease,
+    Contraindication,
+    Medicine,
+)
 
-@admin.register(Tablet)
-class TabletAdmin(admin.ModelAdmin):
+
+@admin.register(MedicineForm)
+class MedicineFormAdmin(admin.ModelAdmin):
     list_display = ("name", "created_at", "updated_at")
+    readonly_fields = ("created_at", "updated_at")
+
+
+@admin.register(MedicineType)
+class MedicineTypeAdmin(admin.ModelAdmin):
+    list_display = ("name", "medicine_form", "created_at", "updated_at")
+    readonly_fields = ("created_at", "updated_at")
+
+
+@admin.register(Disease)
+class DiseaseAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_at", "updated_at")
+    readonly_fields = ("created_at", "updated_at")
+
+
+@admin.register(Contraindication)
+class ContraindicationAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_at", "updated_at")
+    readonly_fields = ("created_at", "updated_at")
+
+
+@admin.register(Medicine)
+class MedicineAdmin(admin.ModelAdmin):
+    list_display = ("name", "medicine_type")
     readonly_fields = ("created_at", "updated_at")
--- apps/pharmacy/models.py
+++ apps/pharmacy/models.py
@@ -1,7 +1,119 @@
 from django.db import models
 
-class Tablet(models.Model):
+
+class MedicineForm(models.Model):
+    LIQUID = "LIQUID"
+    SOLIDE = "SOLIDE"
+    MAGIC = "MAGIC"
+    AEROSOL = "AEROSOL"
+
+    name = models.CharField(
+        verbose_name="Name",
+        max_length=255,
+        choices=[
+            (LIQUID, "Liquid"),
+            (SOLIDE, "Solide"),
+            (MAGIC, "Magic"),
+            (AEROSOL, "Aerosol"),
+        ],
+    )
+
+    created_at = models.DateTimeField(verbose_name="Created at", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="Updated at", auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class MedicineType(models.Model):
+    DROPS = "DROPS"
+    TINCTURE = "TINCTURE"
+    INFUSION = "INFUSION"
+    SYRUP = "SYRUP"
+    SUSPENSION = "SUSPENSION"
+    EMULSION = "EMULSION"
+    CAPSULE = "CAPSULE"
+    TABLET = "TABLET"
+    POWDER = "POWDER"
+    GRANULE = "GRANULE"
+    DRAGEE = "DRAGEE"
+    CARAMEL = "CARAMEL"
+    PENCIL = "PENCIL"
+    CREAM = "CREAM"
+    OINTMENT = "OINTMENT"
+    GEL = "GEL"
+    SUPPOSITORS = "SUPPOSITORS"
+    PASTE = "PASTE"
+
+    name = models.CharField(
+        verbose_name="Name",
+        max_length=255,
+        choices=[
+            (DROPS, "Drops"),
+            (TINCTURE, "Tincture"),
+            (INFUSION, "Infusion"),
+            (SYRUP, "Syrup"),
+            (SUSPENSION, "Suspension"),
+            (EMULSION, "Emulsion"),
+            (CAPSULE, "Capsule"),
+            (TABLET, "Tablet"),
+            (POWDER, "Powder"),
+            (GRANULE, "Granule"),
+            (DRAGEE, "Dragee"),
+            (CARAMEL, "Caramel"),
+            (PENCIL, "Pencil"),
+            (CREAM, "Cream"),
+            (OINTMENT, "Ointment"),
+            (GEL, "Get"),
+            (SUPPOSITORS, "Supppositors"),
+            (PASTE, "Paste"),
+        ],
+    )
+    medicine_form = models.ForeignKey(
+        MedicineForm, verbose_name="Medicine form", on_delete=models.SET_NULL, null=True
+    )
+
+    created_at = models.DateTimeField(verbose_name="Created at", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="Updated at", auto_now=True)
+
+    def get_medicine_form_name(self):
+        return self.medicine_form
+
+    def __str__(self):
+        return self.name
+
+
+class Disease(models.Model):
     name = models.CharField(verbose_name="Name", max_length=255)
+    description = models.TextField(verbose_name="Description", null=True, blank=True)
+
+    created_at = models.DateTimeField(verbose_name="Created at", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="Updated at", auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Contraindication(models.Model):
+    name = models.CharField(verbose_name="Name", max_length=255)
+    description = models.TextField(verbose_name="Description", null=True, blank=True)
+
+    created_at = models.DateTimeField(verbose_name="Created at", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="Updated at", auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Medicine(models.Model):
+    name = models.CharField(verbose_name="Name", max_length=255)
+    medicine_type = models.ForeignKey(
+        MedicineType, verbose_name="Medicine type", on_delete=models.SET_NULL, null=True
+    )
+    disease = models.ManyToManyField(Disease, verbose_name="Diseases")
+    contraindication = models.ManyToManyField(
+        Contraindication, verbose_name="Contrandications", null=True, blank=True
+    )
 
     created_at = models.DateTimeField(verbose_name="Created at", auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name="Updated at", auto_now=True)
